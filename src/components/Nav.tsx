65aa36bbcf87182e0028e5af9405ce1ff9bd3f14
Adam Janicki <adamjanicki2@gmail.com>
2024-08-08 10:58:58 -0700

    new logo

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,6 @@
 import "src/components/nav.css";
 import { UnstyledLink } from "src/components/Link";
+import { ReactComponent as Logo } from "src/images/logo.svg";
 
 type NavlinkProps = {
   to: string;
@@ -14,8 +15,9 @@ const Navlink = (props: NavlinkProps) => (
 
 const Nav = () => (
   <nav className="flex items-center justify-between w-100 nav pv2 ph4">
-    <UnstyledLink className="nav-title" to="/">
-      <span className="desktop">React Playground</span> 🛝
+    <UnstyledLink className="nav-title flex items-center" to="/">
+      <span className="desktop mr2">React Playground</span>
+      <Logo style={{ height: 32 }} />
     </UnstyledLink>
     <ul className="flex items-center link-container ma0">
       <Navlink to="/about/">About</Navlink>
--- src/utils/allowedModules.ts
+++ src/utils/allowedModules.ts
@@ -1,9 +1,11 @@
 import React from "react";
-import * as adamjanickiui from "@adamjanicki/ui";
+import * as ui from "@adamjanicki/ui";
+import * as uiExtended from "@adamjanicki/ui-extended";
 
 const allowedModules = {
   react: React,
-  "@adamjanicki/ui": adamjanickiui,
+  "@adamjanicki/ui": ui,
+  "@adamjanicki/ui-extended": uiExtended,
 } as const;
 
 export default allowedModules;
