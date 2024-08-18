797a5c2cc93bff4112a461f41038d28300cd41f3
Adam Janicki <adamjanicki2@gmail.com>
2024-08-17 21:02:08 -0700

    logo align

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -16,8 +16,8 @@ const Navlink = (props: NavlinkProps) => (
 const Nav = () => (
   <nav className="flex items-center justify-between w-100 nav nav-bs pv2 ph4">
     <UnstyledLink className="nav-title flex items-center" to="/">
-      <span className="desktop mr2">React Playground</span>
       <Logo style={{ height: 32 }} />
+      <span className="desktop ml2">React Playground</span>
     </UnstyledLink>
     <ul className="flex items-center link-container ma0">
       <Navlink to="/about/">About</Navlink>
