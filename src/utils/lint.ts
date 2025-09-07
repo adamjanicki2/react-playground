94138063d1cb8030b21603bf5b0dbfd8eb71c684
Adam Janicki <adamjanicki2@gmail.com>
2025-09-07 11:10:29 -0700

    linting

--- src/utils/lint.ts
+++ src/utils/lint.ts
@@ -1,4 +1,10 @@
-export default function lint(code: string): string {
-  // TODO
-  return code;
+import prettier from "prettier/standalone";
+import babelPlugin from "prettier/plugins/babel";
+import estreePlugin from "prettier/plugins/estree";
+
+export default function lint(code: string): Promise<string> {
+  return prettier.format(code, {
+    parser: "babel",
+    plugins: [babelPlugin, estreePlugin],
+  });
 }
